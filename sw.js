18fd04b9b410c08b9e27cc95bc84626d7d5222ca
shubhtravels <sc1022075@gmail.com>
2026-09-08 16:20:38 +0530

    Upgrade service worker to version 5.0

--- sw.js
+++ sw.js
@@ -1,5 +1,35 @@
-const CACHE='shubh-tour-v3';
-const ASSETS=['./','./index.html','./branding_header.png','./logo.png','./manifest.webmanifest'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
-self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(caches.match(e.request).then(cached=>cached||fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match('./index.html'))));});
+/* Shubh Tour & Travels - Service Worker v5.0 */
+const VERSION = 'shubh-tour-travels-v5.0';
+
+self.addEventListener('install', function () {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function (event) {
+  event.waitUntil(
+    Promise.all([
+      self.clients.claim(),
+      caches.keys().then(function (keys) {
+        return Promise.all(
+          keys.filter(function (key) {
+            return key !== VERSION;
+          }).map(function (key) {
+            return caches.delete(key);
+          })
+        );
+      })
+    ])
+  );
+});
+
+self.addEventListener('fetch', function (event) {
+  if (event.request.method !== 'GET') return;
+
+  event.respondWith(
+    fetch(event.request).catch(function () {
+      return caches.match(event.request).then(function (cached) {
+        return cached || caches.match('./index.html');
+      });
+    })
+  );
+});
